48bbd4d235a3c499bcb8dfe9187725f8ed941121
William Guilherme <wguilherme@securitygeek.io>
2021-09-24 16:59:42 -0700

    Updated Provider

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {
         zpa = {
             version = "1.0.0"
-            source = "zscaler.com/zpa/zpa"
+            source = "github.com/willguibr/zpa_config"
         }
     }
 }
