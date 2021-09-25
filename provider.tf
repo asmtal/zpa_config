e9fce15643bcf72b96f8f8cd5494fb8e234759a1
William Guilherme <wguilherme@securitygeek.io>
2021-09-24 17:04:11 -0700

    Initial Commit

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {
         zpa = {
             version = "1.0.0"
-            source = "github.com/willguibr/zpa_config"
+            source = "terraform.d/plugins/linux_amd64"
         }
     }
 }
